a1c48037025bef4a00e49580f212394d37f0c57b
Cleber Zancanaro <168768247+cleberlz@users.noreply.github.com>
2024-10-01 20:57:12 -0300

    241001.1

---
+++ aula1/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('form').on('submit', function(event){
+        event.stopPropagation();
+        event.preventDefault();
+    });
+    $('input[name=cep]').on('change', function(event){
+        
+    });
+});
