73c3e4d9388031fe19c50742a24e04834fdc11d0
Cleber Zancanaro <168768247+cleberlz@users.noreply.github.com>
2024-10-08 20:38:12 -0300

    241008.1

--- aula2/script.js
+++ aula2/script.js
@@ -1,6 +1,17 @@
 $(document).ready(function () {
-    $.getJSON("./data.json", function (data) {
-        alert(data.name);
+    $("#enviar").on("click", function (event) {
+        $.getJSON("./data.json", function (data) {
+            $("#resultado1").append(data.name + ', ');
+            $("#resultado1").append(data.age + ' anos');
+            $("#resultado1").append('<br>');
+
+            for (var i in data.cars) {
+                $("#resultado1").append(data.cars[i] + '<br>');
+            }
+        });
     });
 
+    $("#limpar").on("click", function (event) {
+        $("div[id^=resultado]").empty();
+    });
 });
