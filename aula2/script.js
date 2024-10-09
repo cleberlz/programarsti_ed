ca66cef7bbe8dbb40ef28e764fda09feb2d7f315
Cleber Zancanaro <168768247+cleberlz@users.noreply.github.com>
2024-10-08 21:59:33 -0300

    241008.2

--- aula2/script.js
+++ aula2/script.js
@@ -12,6 +12,51 @@ $(document).ready(function () {
     });
 
     $("#limpar").on("click", function (event) {
-        $("div[id^=resultado]").empty();
+        $("div[id^=resultado1]").empty();
+    });
+
+    $("#enviar2").on("click", function (event) {
+        $.getJSON("./data2.json", function (data) {
+            $("#resultado2").append("Name: " + data.name + ', ')
+                .append("Idade: " + data.age + ' anos')
+                .append('<br>')
+                .append("Friends:<br>");
+
+            for (var i in data.friends) {
+                $("#resultado2").append((parseInt(i) + 1))
+                    .append(' => ')
+                    .append(data.friends[i].firstName)
+                    .append(' ')
+                    .append(data.friends[i].lastName)
+                    .append('<br>');
+            }
+            $("#resultado2").append("Count: " + data.friends.length)
+                .append('<br>');
+        });
+    });
+
+    $("#limpar2").on("click", function (event) {
+        $("div[id^=resultado2]").empty();
+    });
+
+    $("#enviar3").on("click", function (event) {
+        $.getJSON("./data3.json", function (data) {
+            for (var i in data.results) {
+                $("#resultado3").append(data.results[i].gender);
+                $("#resultado3").append('<br>');
+                $("#resultado3").append(data.results[i].name.title);
+                $("#resultado3").append('<br>');
+                $("#resultado3").append(data.results[i].email);
+                $("#resultado3").append('<br>');
+                $("#resultado3").append(data.results[i].picture.large);
+                $("#resultado3").append('<br>');
+            }
+            $("#resultado3").append(data.info.seed);
+
+        });
+    });
+
+    $("#limpar3").on("click", function (event) {
+        $("div[id^=resultado3]").empty();
     });
 });
