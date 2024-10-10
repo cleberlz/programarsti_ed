566779edd73b7fb38e67325692012e61365786fa
Cleber Zancanaro <168768247+cleberlz@users.noreply.github.com>
2024-10-09 21:21:56 -0300

    dvsdfsdfds

--- aula3/script.js
+++ aula3/script.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
     $.getJSON("https://randomuser.me/api/?results=10&nat=br", function (data) {
-        $("table tbody").empty(); 
+        $("table tbody").empty(); // Limpa o vas s
         
         for (var i = 0; i < data.results.length; i++) {
             var out = "<tr>";
             out += "<td scope='row'>" + (parseInt(i) + 1) + "</td>";
-            out += "<td><!-- <img src='" + data.results[i].picture.thumbnail + "' /> --></td>";
+            out += "<td><img src='" + data.results[i].picture.thumbnail + "' /></td>";
             out += "<td>" + data.results[i].login.username + "</td>";
             out += "<td>" + data.results[i].name.first + "</td>";
             out += "<td>" + data.results[i].name.last + "</td>";
