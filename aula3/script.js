6ba91c325870fa99732808a362f452a80a8e0c4e
Cleber Zancanaro <168768247+cleberlz@users.noreply.github.com>
2024-10-09 21:40:30 -0300

    sfsdgfd

--- aula3/script.js
+++ aula3/script.js
@@ -1,11 +1,14 @@
 $(document).ready(function() {
-    $.getJSON("https://randomuser.me/api/?results=10&nat=br", function (data) {
+    $.getJSON("https://randomuser.me/api/?results=10&nat=br", function(data) {
         $("table tbody").empty(); 
         
         for (var i = 0; i < data.results.length; i++) {
+            var thumbnailUrl = data.results[i].picture.thumbnail;
+            console.log(thumbnailUrl); 
+
             var out = "<tr>";
             out += "<td scope='row'>" + (parseInt(i) + 1) + "</td>";
-            out += "<td><img src='" + data.results[i].picture.thumbnail + "' /></td>";
+            out += "<td><img src='" + thumbnailUrl + "' alt='Thumbnail' onerror='this.onerror=null;this.src=\"https://via.placeholder.com/50\";' /></td>";
             out += "<td>" + data.results[i].login.username + "</td>";
             out += "<td>" + data.results[i].name.first + "</td>";
             out += "<td>" + data.results[i].name.last + "</td>";
